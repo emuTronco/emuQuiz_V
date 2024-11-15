5c653ff2b831a70846239e8753b10b6fb0e87458
David Patron Fernandez <tronez98@gmail.com>
2024-11-16 00:55:57 +0100

    16/11/2024

--- src/index.js
+++ src/index.js
@@ -1,122 +1,41 @@
 const contenedor = document.getElementById("test");
-const botonRes = document.getElementById("boton");
+const botonRes = document.getElementById("botonResultado");
 const resultadoTest = document.getElementById("resultado");
 
 const preguntas = [
   {
-    pregunta: "1. ¿Donde le gustaria vivir a Joseda?",
+    pregunta: "1. ¿Por cuantas ciudades pasan Sean y Daniel en Life is Strange 2?",
     respuestas: {
-      a: "Grieta del invocador",
-      b: "Isekai",
-      c: "Sanse",
-      d: "Las Vegas"
-    },
-    respuestaCorrecta: "b"
-  },
-  {
-    pregunta: "2. ¿Cuantos dias vamos a Italia?",
-    respuestas: {
-      a: "7",
-      b: "8",
-      c: "9",
-      d: "10"
-    },
-    respuestaCorrecta: "c"
-  },
-  {
-    pregunta:
-      "3. ¿Cuantas colas tiene Naruto en la pelea contra Orochimaru en el Puente del cielo y la Tierra?",
-    respuestas: {
-      a: "1",
-      b: "2",
-      c: "4",
+      a: "2",
+      b: "4",
+      c: "5",
       d: "6"
-    },
-    respuestaCorrecta: "c"
-  },
-  {
-    pregunta: "4. ¿De que restaurante es mas probable que salgas rodando?",
-    respuestas: {
-      a: "Tierra",
-      b: "McDonalds",
-      c: "Chino",
-      d: "Fast & Good"
-    },
-    respuestaCorrecta: "a"
-  },
-  {
-    pregunta:
-      "5. Te has quedado calvo, ¿cual es probablemente tu juego favorito?",
-    respuestas: {
-      a: "Persona",
-      b: "Zelda",
-      c: "Genshin Impact",
-      d: "Pokemon"
-    },
-    respuestaCorrecta: "c"
-  },
-  {
-    pregunta: "6. ¿Cuantos emuTronez es capaz de levantar Asel?",
-    respuestas: {
-      a: "Solo 1 :(",
-      b: "2",
-      c: "3 (Al menos)",
-      d: "100"
-    },
-    respuestaCorrecta: "c"
-  },
-  {
-    pregunta: "7. ¿Donde está la casa de Noe, la amiga de Alex?",
-    respuestas: {
-      a: "Badalona",
-      b: "Matadepera",
-      c: "Terrasa",
-      d: "Sant Boi de Llobregat"
-    },
-    respuestaCorrecta: "b"
-  },
-  {
-    pregunta: "8. ¿Quien es la emperatriz de Ixaocan?",
-    respuestas: {
-      a: "Nidalee",
-      b: "Qiyana",
-      c: "Karma",
-      d: "Cassiopeia"
-    },
-    respuestaCorrecta: "b"
-  },
-  {
-    pregunta: "9. ¿Quien tiene el record de carreras abandonadas?",
-    respuestas: {
-      a: "Félix",
-      b: "emuTronez",
-      c: "Alex",
-      d: "Adri"
-    },
-    respuestaCorrecta: "a"
-  },
-  {
-    pregunta: "10. ¿Quien tiene mas éxito con las minitas?",
-    respuestas: {
-      a: "Alex",
-      b: "emuTronez",
-      c: "Adri",
-      d: "Joseda"
-    },
-    respuestaCorrecta: "b"
-  },
-  {
-    pregunta: "11. ¿Cual es el segundo mejor campeón de emuTronez?",
-    respuestas: {
-      a: "Vladimir",
-      b: "Tahm Kench",
-      c: "Fizz",
-      d: "Yone"
     },
     respuestaCorrecta: "d"
   },
   {
-    pregunta: "12. [...], por tu culo se me mete",
+    pregunta: "2. ¿Cómo se llama el Pokemon cuya categoria es Zorro pillo?",
+    respuestas: {
+      a: "Fennekin",
+      b: "Nickit",
+      c: "Zorua",
+      d: "Vulpix"
+    },
+    respuestaCorrecta: "c"
+  },
+  {
+    pregunta:
+      "3. ¿Quién es el ser vivo mas dormilón del mundo?",
+    respuestas: {
+      a: "Koala",
+      b: "Jaquito",
+      c: "Gatita malvada",
+      d: "Lironcito"
+    },
+    respuestaCorrecta: "d"
+  },
+  {
+    pregunta: "4. [...], por tu culo se me mete",
     respuestas: {
       a: "3",
       b: "7",
@@ -127,35 +46,116 @@ const preguntas = [
   },
   {
     pregunta:
-      "13. ¿Cuál es el campeón con el que es mas probable que Joseda se estampe?",
+      "?aedi adavlam atse odis árbah néiuq eD¿ !oditrevni odis ah otxet lE¡ !on hO¡ .5",
     respuestas: {
-      a: "Sejuani",
-      b: "Kayn",
-      c: "Kha'zix",
-      d: "Ekko"
+      a: "zenorTume",
+      b: "ordnajelA",
+      c: "leuqaR",
+      d: "adavlam atitaG"
+    },
+    respuestaCorrecta: "a"
+  },
+  {
+    pregunta: "6. ¿En que tecla se encuentra la habilidad de Yuumi \"Presteza gatuna\"?",
+    respuestas: {
+      a: "Q",
+      b: "W",
+      c: "E",
+      d: "R"
+    },
+    respuestaCorrecta: "c"
+  },
+  {
+    pregunta: "7. ¿Cómo se llama el restaurante donde fuimos a comer barbacoa coreana?",
+    respuestas: {
+      a: "Kim's Barbacoa Coreana",
+      b: "Kuili Korean BBQ",
+      c: "Qiquihary BBQ",
+      d: "Gogi gui"
+    },
+    respuestaCorrecta: "b"
+  },
+  {
+    pregunta: "8. ¿Cómo se dice \"Vallado\" en alemán?",
+    respuestas: {
+      a: "Zaun",
+      b: "Braum",
+      c: "Freljord",
+      d: "Shurima"
+    },
+    respuestaCorrecta: "a"
+  },
+  {
+    pregunta: "9. ¿Cuantas plantas tiene el castillo de Bellver?",
+    respuestas: {
+      a: "1",
+      b: "2",
+      c: "3",
+      d: "4"
+    },
+    respuestaCorrecta: "b"
+  },
+  {
+    pregunta: "10. ¿Quién el amigo de la infancia de Punpun con el que se reencuentra al final del manga?",
+    respuestas: {
+      a: "Tanaka Aiko",
+      b: "Harumi Shuntarou",
+      c: "Nanjo Sachi",
+      d: "Onodera Yuichi"
+    },
+    respuestaCorrecta: "b"
+  },
+  {
+    pregunta: "11. ¿En que juego de Yakuza tienes que ayudar a un artista callejero que hace de estatua a que vaya al baño y no le vea la gente?",
+    respuestas: {
+      a: "0",
+      b: "Kiwami",
+      c: "Like a Dragon",
+      d: "Infinite Wealth"
+    },
+    respuestaCorrecta: "a"
+  },
+  {
+    pregunta: "12. ¿Cual es el célebre lugar favorito de los niños que se encuentra en Sanse?",
+    respuestas: {
+      a: "Plaza de toros La tercera",
+      b: "Micrópolix",
+      c: "Dehesa Boyal",
+      d: "Plaza norte 2"
+    },
+    respuestaCorrecta: "b"
+  },
+  {
+    pregunta:
+      "13. ¿Quién es el presidente del consejo estudiantil en Persona 3?",
+    respuestas: {
+      a: "Hidetoshi Odagiri",
+      b: "Akihiko Sanada",
+      c: "Mitsuru Kirijo",
+      d: "Chihiro Fushimi"
     },
     respuestaCorrecta: "c"
   },
   {
     pregunta:
-      "?aedi adavlam atse odis árbah neiuq eD¿ !oditrevni odis ah otxet lE¡ !on hO¡ .41",
+      "14. ¿Cuál es la bebida láctea chocolateada más deliciosa de toda España?",
     respuestas: {
-      a: "zenorTume",
-      b: "aruaL",
-      c: "oiraM",
-      d: "udE"
+      a: "Laccao",
+      b: "Cacaolat",
+      c: "Batido OKEY cacao",
+      d: "Batido Puleva chocolate"
     },
     respuestaCorrecta: "a"
   },
   {
-    pregunta: "15. ¿Quien tiene mas probabilidades de acabar en el paro?",
+    pregunta: "15. ¿Cuántas veces se han visto la gatita y el lironcito?",
     respuestas: {
-      a: "Mario",
-      b: "Laura",
-      c: "Asel",
-      d: "Alex"
+      a: "1",
+      b: "2",
+      c: "3",
+      d: "4"
     },
-    respuestaCorrecta: "b"
+    respuestaCorrecta: "c"
   }
 ];
 
@@ -168,7 +168,7 @@ function mostrarTest() {
     for (const letraRespuesta in preguntaActual.respuestas) {
       respuestas.push(
         `
-        <div style="display: flex">
+        <div style="display: flex" class = opciones>
           <input type="radio" name="${numeroDePregunta}" value="${letraRespuesta}" />
           <label>${preguntaActual.respuestas[letraRespuesta]}</label>
           </div>
@@ -218,7 +218,7 @@ function mostrarResultado() {
   else
     resultadoTest.innerHTML =
       "¡Enhorabuena, has acertado todas las preguntas!" +
-      "Ahora, apunta la primera letra/número de cada respuesta correcta.";
+      " Ahora, apunta la primera letra/número de cada respuesta correcta.";
 }
 
 botonRes.addEventListener("click", mostrarResultado);
